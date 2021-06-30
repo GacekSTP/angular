8c3b402075926b0ab74afc8bcf2231ac669dabc7
GacekSTP <86727748+GacekSTP@users.noreply.github.com>
2021-06-30 17:48:32 +0200

    Add files via upload

---
+++ projekt/src/environments/environment.ts
@@ -0,0 +1,28 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBJh7LdRCClGdeL_M0bCt5FYZaI1mBetjQ",
+    authDomain: "projekt-453b7.firebaseapp.com",
+    projectId: "projekt-453b7",
+    storageBucket: "projekt-453b7.appspot.com",
+    messagingSenderId: "34517479663",
+    appId: "1:34517479663:web:8ed573b8434d485f8eab1a",
+    measurementId: "G-ZFD7S1YC8V"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
